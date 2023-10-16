cffb8dcf8a4bb080a2c566de8fb82cd87ff6f124
Jeffwonderboiz <jefferyhuca@gmail.com>
2023-10-16 17:55:09 -0400

    player controller bug fix

--- dark city test scene/Assets/Scripts/CharacterController.cs
+++ dark city test scene/Assets/Scripts/CharacterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using static UnityEditor.Searcher.SearcherWindow.Alignment;
+//using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class CharacterController : MonoBehaviour
 {
